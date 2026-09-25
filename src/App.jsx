220b2ff79a1fc146de63c4787859e347d603e1a6
Seo-JS0823 <seong0093571@naver.com>
2026-09-26 00:16:05 +0900

    feat: 회원가입 페이지 디자인 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MatchingPage from './pages/patient/MatchingPage'
 import MyPage from './pages/patient/MyPage'
 import NotFoundPage from './pages/NotFoundPage'
 import SchedulePage from './pages/patient/SchedulePage'
+import SignupPage from './pages/user/SignupPage'
 import { PATHS } from './constants/paths'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         {/* 헤더/네브바 없는 화면 */}
         <Route path={PATHS.login} element={<LoginPage />} />
+        <Route path={PATHS.signup} element={<SignupPage />} />
 
         {/* 헤더 + 네브바가 붙는 화면 */}
         <Route element={<AppLayout />}>
--- src/pages/user/SignupPage.jsx
+++ src/pages/user/SignupPage.jsx
@@ -1,13 +1,23 @@
 import { useNavigate } from "react-router-dom";
-import { PATHS } from "../../constants/paths";
-import BottomBar from "../../components/layout/BottomBar";
+import Header, { HeaderSpacer } from '../../components/layout/Header'
+import BottomBar from "../../components/layout/BottomBar"
+import styles from './SignupPage.module.css'
 
 function SignupPage() {
   const navigate = useNavigate()
 
   return(
-    <div>
-      
+    <div className={styles.page}>
+      <Header title="회원가입" logo={null} showBack onBack={() => navigate(-1)}/>
+      <HeaderSpacer />
+
+      <h1 className={styles.title}>가입 유형을 선택하세요</h1>
+      <p className={styles.description}>
+        보다 정확한 서비스 매칭을 위해 알맞은 유형을 선택해주세요.
+      </p>
+
+      <BottomBar />
+
     </div>
   )
 }
