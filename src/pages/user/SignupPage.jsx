59ca891bd114df8af4a763249b3becb49c4dbfe4
Seo-JS0823 <seong0093571@naver.com>
2026-09-26 00:02:20 +0900

    feat: 회원가입 페이지 파일 준비

---
+++ src/pages/user/SignupPage.jsx
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+import { PATHS } from "../../constants/paths";
+import BottomBar from "../../components/layout/BottomBar";
+
+function SignupPage() {
+  const navigate = useNavigate()
+
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignupPage
