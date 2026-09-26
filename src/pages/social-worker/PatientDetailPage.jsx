029bc46614c07e6df28ac6b1edf142188e5ec781
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-26 12:39:33 +0900

    feat: 사회복지사 페이지 기본 골격 추가

---
+++ src/pages/social-worker/PatientDetailPage.jsx
@@ -0,0 +1,10 @@
+function PatientDetailPage() {
+    return (
+        <div>
+            <h1>Patient Detail Page</h1>
+            <p>Details about the patient will be displayed here.</p>
+        </div>
+    );
+}
+
+export default PatientDetailPage;
---
+++ src/pages/social-worker/ScheduleDetailPage.jsx
@@ -0,0 +1,10 @@
+function ScheduleDetailPage() {
+    return (
+        <div>
+            <h1>Schedule Detail Page</h1>
+            <p>Details about the schedule will be displayed here.</p>
+        </div>
+    );
+}
+
+export default ScheduleDetailPage;
