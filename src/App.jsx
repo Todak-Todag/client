51b7ffc7f29d585db33bf526bab34ed5fae58c37
hansolChoi29 <180262795+hansolChoi29@users.noreply.github.com>
2026-09-26 14:07:31 +0900

    feat: 사회복지사 메인화면 구현 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import AppLayout from './layouts/AppLayout'
-import HomePage from './pages/patient/HomePage'
-import LoginPage from './pages/LoginPage'
-import MatchingPage from './pages/patient/MatchingPage'
-import MyPage from './pages/patient/MyPage'
-import NotFoundPage from './pages/NotFoundPage'
-import SchedulePage from './pages/patient/SchedulePage'
-import { PATHS } from './constants/paths'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import HomePage from "./pages/patient/HomePage";
+import LoginPage from "./pages/LoginPage";
+import MatchingPage from "./pages/patient/MatchingPage";
+import MyPage from "./pages/patient/MyPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import SchedulePage from "./pages/patient/SchedulePage";
+import { PATHS } from "./constants/paths";
+import SocialWorkerHomePage from "./pages/social-worker/SocialWorkerHomePage";
 
 function App() {
   return (
@@ -21,13 +22,16 @@ function App() {
           <Route path={PATHS.schedule} element={<SchedulePage />} />
           <Route path={PATHS.matching} element={<MatchingPage />} />
           <Route path={PATHS.my} element={<MyPage />} />
+
+          <Route path="/social-worker" element={<SocialWorkerHomePage />} />
+
           <Route path="/404" element={<NotFoundPage />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
